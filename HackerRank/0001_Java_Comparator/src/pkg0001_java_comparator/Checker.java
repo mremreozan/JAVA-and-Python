32ffde842f0e5558f32fd2375761cfd5ac910f8c
emre ozan <mremreozan@gmail.com>
2019-12-07 15:41:01 +0100

    add hackerrank java comparator

---
+++ HackerRank/0001_Java_Comparator/src/pkg0001_java_comparator/Checker.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg0001_java_comparator;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Checker implements Comparator<Player> {
+    public int compare (Player p1, Player p2){
+        if(p1.score == p2.score){
+            return p1.name.compareTo(p2.name);
+        }
+        else{
+            return p2.score-p1.score;
+        }
+   
+    }    
+}
