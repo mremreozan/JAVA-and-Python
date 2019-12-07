0d4f34d860b8c0ee1629e20d48e1ff2355f4ddb6
emre ozan <mremreozan@gmail.com>
2019-12-07 15:41:01 +0100

    add hackerrank java comparator

---
+++ HackerRank/0001_Java_Comparator/src/pkg0001_java_comparator/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg0001_java_comparator;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+    
+}
---
+++ HackerRank/0001_Java_Comparator/src/pkg0001_java_comparator/Player.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg0001_java_comparator;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Player {
+    String name;
+    int score;
+    
+    public Player(String name, int score){
+        this.name = name;
+        this.score=score;
+    }
+}
