08ef626851c9d98b78ce30bdf163db8191ed15ef
Emre Ozan <mremreozan@gmail.com>
2020-05-31 18:42:36 +0200

    add python file

---
+++ Python/Datacamp/test/function_for_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 17:38:07 2020
+
+@author: Utilisateur
+"""
+
+def convert_to_int(string_with_comma):
+    # Fix this line so that it returns an int, not a str
+    return string_with_comma.replace(",", "")
+
+def convert_to_int2(string_with_comma):
+    # Fix this line so that it returns an int, not a str
+    return int(string_with_comma.replace(",", ""))
