edc82849efb8353b11c2a37c4826f16cc1ebb2d7
Emre Ozan <mremreozan@gmail.com>
2020-05-31 18:44:07 +0200

    emre

---
+++ Python/Datacamp/test/pytest_function.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 17:35:46 2020
+
+@author: Utilisateur
+"""
+import function_for_test as funcs
+
+# Complete the unit test name by adding a prefix
+def test_convert_to_int():
+  # Complete the assert statement
+  assert funcs.convert_to_int("2,081") == 2081
+  
+def test_convert_to_int2():
+  # Complete the assert statement
+  assert funcs.convert_to_int2("2,081") == 2081
