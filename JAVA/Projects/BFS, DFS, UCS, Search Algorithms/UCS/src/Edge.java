4461edada8a0f84bf180921137099b60eabb4650
Emre Ozan <mremreozan@gmail.com>
2020-06-09 17:50:57 +0200

    UCS

---
+++ JAVA/Projects/BFS, DFS, UCS, Search Algorithms/UCS/src/Edge.java
@@ -0,0 +1,13 @@
+
+
+public class Edge {
+	public final double cost;
+    public final Node target;
+
+    public Edge(Node targetNode, double costVal){
+        cost = costVal;
+        target = targetNode;
+    }
+
+}
+    
