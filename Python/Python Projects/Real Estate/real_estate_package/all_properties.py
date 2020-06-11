227513e21eaf600c144b5bb2650f55b154c8afde
Emre Ozan <mremreozan@gmail.com>
2020-06-12 00:00:20 +0200

    real estate inheritance python project

---
+++ Python/Python Projects/Real Estate/real_estate_package/all_properties.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Utilisateur
+"""
+from .purchase_and_rental import *
+from .apartment_and_house import *
+
+class HouseRental(Rental, House):
+    def prompt_init():
+        init = House.prompt_init()
+        init.update(Rental.prompt_init())
+        return init
+    
+    prompt_init = staticmethod(prompt_init)
+
+class ApartmentRental(Rental, Apartment):
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Rental.prompt_init())
+        return init
+    prompt_init = staticmethod(prompt_init)
+
+class ApartmentPurchase(Purchase, Apartment):
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Purchase.prompt_init())
+        return init
+    prompt_init = staticmethod(prompt_init)
+
+class HousePurchase(Purchase, House):
+    def prompt_init():
+        init = House.prompt_init()
+        init.update(Purchase.prompt_init())
+        return init
+    prompt_init = staticmethod(prompt_init)
